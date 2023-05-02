f7867eec55b07a61443d9815448e21436e28bce2
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2023-05-01 23:18:24 -0600

    Acquired API key. Updated constants in script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,11 @@
-const apiLink= "";
-const img_path= "";
-const searchAPI= "";
+const apiLink= "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=132f4127b4d2b00db3532a9a9560679b&page=1";
+const img_path= "https://image.tmdb.org/t/p/w1280";
+const searchAPI= "https://api.themoviedb.org/3/search/movie?&api_key=132f4127b4d2b00db3532a9a9560679b&query=";
+
+const main= document.getElementById("section");
+const form= document.getElementById("form");
+const query= document.getElementById("query");
+
+function returnMovies(){
+    
+}
