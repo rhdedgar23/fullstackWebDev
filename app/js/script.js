b15caf233b15775140630747f6d4d9eb77399010
rhdedgar23 <96807850+rhdedgar23@users.noreply.github.com>
2023-05-07 22:48:46 -0600

    Added returnMovies function.
    Added comments about the functionality of fetch(), json() methods.

--- app/js/script.js
+++ app/js/script.js
@@ -6,6 +6,19 @@ const main= document.getElementById("section");
 const form= document.getElementById("form");
 const query= document.getElementById("query");
 
-function returnMovies(){
+function returnMovies(url){
+    //Simplest form of fetch() has one argument (path to the resource you want to fetch) which returns
+    //a promise that resolves with a Response object.
+    //The response object doesnt directly contain the actual JSON response body. 
+    //Its a representation of the entire HTTP response. 
+    //To extract the JSON body content from the Response object, we use the json() method. 
+    //It returns a second promise that resolves with the result of parsing the response body text as JSON.
     
+    
+    fetch(url).then(res => res.json()).then(function(data){//fetch url; extract response object; pass response object into a function to extract data
+        console.log(data.results);//with this data
+        data.results.forEach(element => {
+                const div= document.createElement('div');
+        });
+    });
 }
